dc4b49f36d8c44a56230034a0200f3cff38ed1d9
Rico <rico@toggl.com>
2023-09-24 19:09:09 +0800

    feat (TogglTrackTags): add

--- src/TogglTrack.js
+++ src/TogglTrack.js
@@ -2,12 +2,14 @@ import { TogglTrackUsers } from './TogglTrackUsers'
 import { TogglTrackProjects } from './TogglTrackProjects'
 import { TogglEmitter } from './TogglEmitter'
 import { TogglTrackClients } from './TogglTrackClients'
+import { TogglTrackTags } from './TogglTrackTags'
 
 export class TogglTrack {
   constructor (options) {
     this.clients = new TogglTrackClients(options)
     this.projects = new TogglTrackProjects(options, this.clients)
     this.users = new TogglTrackUsers(options)
+    this.tags = new TogglTrackTags(options)
     this.cache = options.cache
   }
 
---
+++ src/TogglTrackTags.js
@@ -0,0 +1,64 @@
+import { TogglTrackAPI } from './TogglTrackAPI'
+import { objectToQueryString } from './utils'
+
+export class TogglTrackTags extends TogglTrackAPI {
+  async getAll (options = {}) {
+    const queryString = objectToQueryString(options)
+
+    const tags = await this.restRequest(
+      'GET',
+      '/api/v9/workspaces/{{workspace_id}}/tags?' + queryString,
+      null,
+      'json',
+      'TogglTrackClients.getAll()'
+    )
+
+    return tags
+  }
+
+  async getByName (name) {
+    const tags = await this.getAll()
+
+    for (const tag of tags) {
+      if (tag.name === name) {
+        return tag
+      }
+    }
+
+    return false
+  }
+
+  async create (name) {
+    const result = await this.restRequest(
+      'POST',
+      '/api/v9/workspaces/{{workspace_id}}/tags',
+      { name },
+      'json',
+      'TogglTrackTags.create()'
+    )
+
+    this.emit('TogglTrackTags.create()', `Creating tag ${name}`)
+
+    return result
+  }
+
+  async delete (name) {
+    const tag = await this.getByName(name)
+
+    if (tag === false) {
+      return false
+    }
+
+    const result = await this.restRequest(
+      'PATCH',
+      '/api/v9/workspaces/5972963/tags',
+      { delete: [tag.id] },
+      'text',
+      'TogglTrackTags.delete()'
+    )
+
+    this.emit('TogglTrackTags.delete()', `Deleting tag ${name}`)
+
+    return result
+  }
+}
